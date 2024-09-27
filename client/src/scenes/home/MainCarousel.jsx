1615e286d876b3151431988ae1f783a8447429ae
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-09-27 18:36:29 +0300

    Fixed some deploy build errors

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,8 +1,5 @@
-import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
-import { Carousel } from "react-responsive-carousel";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
 import { useTranslation } from "react-i18next";
 
@@ -16,7 +13,6 @@ const MainCarousel = () => {
     <Box key={`image`}>
       <img
         src={heroTextureImport}
-        alt={`image`}
         style={{
           width: "100%",
           height: "700px",
