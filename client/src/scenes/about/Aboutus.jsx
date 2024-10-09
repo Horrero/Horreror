c6be12da097688bb9a31291571c5bd4ad7bbd317
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-10-09 19:36:22 +0300

    I wanna kill myself

---
+++ client/src/scenes/about/Aboutus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Aboutus = () => {
+  return (
+    <div style={{color: "white", marginTop: "60px"}}>
+      Zdrastii
+    </div>
+  )
+}
+
+export default Aboutus;
--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -4,13 +4,42 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useTranslation } from "react-i18next";
 
 import heroTextureImport from "../../assets/main.png";
-import line1 from "../../assets/line 1.png";
-import line2 from "../../assets/line 2.png";
+import bigLines from "../../assets/bigLines.png";
+import bigLinesMini from "../../assets/bigLines-mini.png";
 import line3 from "../../assets/line 3.png";
+import { useEffect, useState, useRef } from "react";
 
 const MainCarousel = () => {
+  const canvas = useRef();
   const { t } = useTranslation();
   // const isNonMobile = useMediaQuery("(min-width:600px");
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      console.log(window.innerWidth); // Logs the current width
+    };
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const ctx = canvas.current.getContext('2d');
+    const img = new Image();
+    img.src = bigLines; // Assuming bigLines is a URL or path to the image
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, 100, 100);
+    };
+  }, []);
 
   return (
     <Box key={`image`}>
@@ -27,29 +56,17 @@ const MainCarousel = () => {
           }}
         />
 
-        <img
-          src={line2}
-          alt=""
-          style={{
-            position: "relative",
-            width: "100%",
-            minWidth: "1100px",
-            bottom: "140px",
-            zIndex: "0"
-          }}
-        />
-        <img
-          src={line1}
+        {/* <img
+          src={windowWidth < 800 ? bigLinesMini : bigLines}
           alt=""
           style={{
             position: "absolute",
-            width: "100%",
-            minWidth: "1100px",
-            bottom: "240px",
-            left: "0",
-            zIndex: "0"
+            left: "0"
           }}
-        />
+        /> */}
+
+        <canvas ref={canvas} width={window.innerWidth} height={windowWidth < 800 ? 300 : 549} style={{position: "absolute", width: "100%", top: "0", left: "0", zIndex: "100"}}></canvas>
+        
         <img
           src={line3}
           alt=""
