1447aa4416534cf2882f22dc7a0c763fcc79ad11
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-10-16 19:22:29 +0300

    Made it to be a carosel

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -1,34 +1,63 @@
 import { Box, Button, Typography } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { shades } from "../../theme";
 import { useTranslation } from "react-i18next";
 
+// Import your images
 import heroTextureImport from "../../assets/main.png";
+import secondImage from "../../assets/image 6.png"; // Add your second image here
 
 const MainCarousel = () => {
   const { t } = useTranslation();
 
   return (
-    <Box key={`image`}>
-      <div style={{width: "100%", height: "700px"}}>
-        <img
-          src={heroTextureImport}
-          alt=""
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            backgroundAttachment: "fixed",
-            zIndex: "-2"
-          }}
-        />
+    <Box>
+      {/* Carousel Component */}
+      <Carousel
+        autoPlay
+        interval={3000} // Time between slide transitions (in milliseconds)
+        infiniteLoop
+        showThumbs={false} // Hides thumbnail images
+        showStatus={false} // Hides status indicator
+        showIndicators={false} // Hides the indicators (dots)
+        stopOnHover={false} // Continues sliding even when hovered
+      >
+        {/* First Image */}
+        <div style={{ width: "100%", height: "700px" }}>
+          <img
+            src={heroTextureImport}
+            alt="First Slide"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+              zIndex: "-2",
+            }}
+          />
+        </div>
 
-      </div>
+        {/* Second Image */}
+        <div style={{ width: "100%", height: "700px" }}>
+          <img
+            src={secondImage}
+            alt="Second Slide"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+              zIndex: "-2",
+            }}
+          />
+        </div>
+      </Carousel>
+
+      {/* Overlay Text and Button */}
       <Box
         color="white"
         borderRadius="1px"
         textAlign="center"
-        // backgroundColor="rgba(0, 0, 0, 0.4)"
         position="absolute"
         top="0"
         bottom="150px"
@@ -42,14 +71,9 @@ const MainCarousel = () => {
           Horreror
         </Typography>
 
-        <Typography
-          fontWeight="bold"
-        >
-          {t('quote')}
-        </Typography>
+        <Typography fontWeight="bold">{t("quote")}</Typography>
 
         <Button
-          m="30"
           sx={{
             backgroundColor: "#ad1c11",
             color: "white",
@@ -66,7 +90,7 @@ const MainCarousel = () => {
             document.location.href = "#shopping-list";
           }}
         >
-          {t('orderNow')}
+          {t("orderNow")}
         </Button>
       </Box>
     </Box>
