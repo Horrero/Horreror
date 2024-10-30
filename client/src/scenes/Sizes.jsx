f65fa3cadd67aefd3cfd56fc1c43e12af2655689
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-10-30 17:37:07 +0200

    Added content to Sizes page

--- client/src/scenes/Sizes.jsx
+++ client/src/scenes/Sizes.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import image from "../assets/sizes.png";
+
 const Sizes = () => {
-  return (
-    <div>
-      
+return (
+    <div style={{ textAlign: 'center', margin: '80px auto 0' }}>
+            <img 
+                    src={image} 
+                    alt="Sizes" 
+                    style={{ 
+                        display: 'block', 
+                        margin: '0 auto', 
+                        maxWidth: '80%',
+                        height: 'auto' 
+                    }} 
+            />
     </div>
-  )
+)
 }
 
-export default Sizes
+export default Sizes;
