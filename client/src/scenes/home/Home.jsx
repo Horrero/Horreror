279aba4fe12c9bb2e27f9933759d2c3d890a4226
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-11-22 16:05:45 +0200

    🔧 Move currentDate declaration inside useEffect for improved accuracy in date checks

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -5,18 +5,18 @@ import ShoppingList from "./ShoppingList";
 // import Subscribe from "./Subscribe";
 
 const Home = () => {
-  const currentDate = new Date();
   const startDate = new Date('2024-11-22T18:00:00');
   const endDate = new Date('2024-11-29T18:00:00');
   const [isOpen, setIsOpen] = useState(false);
-
+  
   useEffect(() => {
+    const currentDate = new Date();
     if (currentDate >= startDate && currentDate <= endDate) {
       setIsOpen(true);
     } else {
       setIsOpen(false);
     }
-  }, [currentDate, startDate, endDate]);
+  }, [startDate, endDate]);
 
   return (
     <div className="home">
