fe38a9b9f4bdc55756e59106061e335b6967f21e
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-12-24 17:52:55 +0200

    🎅🎄❄ Add second image for hoodie sizes in Sizes component

--- client/src/scenes/Sizes.jsx
+++ client/src/scenes/Sizes.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 
 import image from "../assets/sizes.png";
+import image2 from "../assets/sizesHoodies.png";
 
 const Sizes = () => {
-return (
-    <div style={{ textAlign: 'center', margin: '80px auto 0' }}>
+    return (
+        <div style={{ textAlign: 'center', margin: '80px auto 0' }}>
             <img 
-                    src={image} 
-                    alt="Sizes" 
-                    style={{ 
-                        display: 'block', 
-                        margin: '0 auto', 
-                        maxWidth: '80%',
-                        height: 'auto' 
-                    }} 
+                src={image} 
+                alt="Sizes" 
+                style={{ 
+                    display: 'block', 
+                    margin: '0 auto', 
+                    maxWidth: '80%',
+                    height: 'auto' 
+                }} 
             />
-    </div>
-)
+            <img 
+                src={image2} 
+                alt="Sizes for hoodies" 
+                style={{ 
+                    display: 'block', 
+                    margin: '0 auto', 
+                    maxWidth: '80%',
+                    height: 'auto' 
+                }} 
+            />
+        </div>
+    )
 }
 
 export default Sizes;
